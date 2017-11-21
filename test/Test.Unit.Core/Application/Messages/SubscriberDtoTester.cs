debf002c495160f0d3af4284b047aa2039509eab
Walter Pinson <walter@walterpinson.com>
2017-11-21 11:36:33 -0500

    Add a test harness for the SubscriberDto. Add some very simple tests to it.  Ordinarily, you would not test simple types in this way.  It is being done here for instructive purposes, only.

---
+++ test/Test.Unit.Core/Application/Messages/SubscriberDtoTester.cs
@@ -0,0 +1,52 @@
+namespace Test.Unit.Core.Application.Messages
+{
+    using System;
+    using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SubscriberDtoTester
+    {
+        [Test]
+        public void CanCreateSubscriberDto()
+        {
+            // ARRANGE
+
+            // ACT
+            var subjectUnderTest = new SubscriberDto();
+
+            // ASSERT
+            Assert.That(subjectUnderTest, Is.TypeOf(typeof(SubscriberDto)));
+            Assert.That(subjectUnderTest, Is.InstanceOf(typeof(SubscriberDto)));
+        }
+
+        [Test]
+        public void CanSetSubscriberId()
+        {
+            // ARRANGE
+            var expectedId = Guid.NewGuid();
+            var subjectUnderTest = new SubscriberDto();
+
+            // ACT
+            subjectUnderTest.Id = expectedId;
+
+            // ASSERT
+            Assert.That(subjectUnderTest.Id, Is.EqualTo(expectedId));
+        }
+
+        [Test]
+        public void CanSetSubscriberEmailAddress()
+        {
+            // ARRANGE
+            var expectedEmailAddress = "mickey.mouse@disnet.com";
+            var subjectUnderTest = new SubscriberDto();
+
+            // ACT
+            subjectUnderTest.EmailAddress = expectedEmailAddress;
+
+            // ASSERT
+            Assert.That(subjectUnderTest.EmailAddress, Is.EqualTo(expectedEmailAddress));
+        }
+    }
+}
