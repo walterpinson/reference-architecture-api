c4b2e2c6a611b565909baf175a515c1774f7eac6
Walter Pinson <walter@walterpinson.com>
2017-11-22 16:39:57 -0500

    Implement NoteTaker service constructor. Update the CanConstructNoteTaker unit test so that it accounts for the new NoteTaker dependencies that are passed into the constructor.

--- src/Infrastructure.Server/NoteTaker.cs
+++ src/Infrastructure.Server/NoteTaker.cs
@@ -4,9 +4,26 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.Server
     using System.Collections.Generic;
     using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
     using CompanyName.Notebook.NoteTaking.Core.Application.Services;
+    using CompanyName.Notebook.NoteTaking.Core.Domain.Factories;
+    using CompanyName.Notebook.NoteTaking.Core.Domain.Services;
 
     public class NoteTaker : INoteTaker
     {
+        private readonly INoteFactory _noteFactory;
+        private readonly ISubscriberFactory _subscriberFactory;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public NoteTaker(
+            INoteFactory noteFactory,
+            ISubscriberFactory subscriberFactory,
+            ICategoryRepository categoryRepository
+        )
+        {
+            _noteFactory = noteFactory ?? throw new ArgumentNullException(nameof(noteFactory));
+            _subscriberFactory = subscriberFactory ?? throw new ArgumentNullException(nameof(subscriberFactory));
+            _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+        
         public NoteDto ReadCategorizedNote(Guid categoryId, Guid noteId)
         {
             throw new NotImplementedException();
--- test/Test.Unit.Infrastructure.Server/NoteTakerTester.cs
+++ test/Test.Unit.Infrastructure.Server/NoteTakerTester.cs
@@ -3,6 +3,7 @@ namespace Test.Unit.Infrastructure.Server
     using System;
     using CompanyName.Notebook.NoteTaking.Core.Application.Services;
     using CompanyName.Notebook.NoteTaking.Core.Domain.Factories;
+    using CompanyName.Notebook.NoteTaking.Core.Domain.Services;
     using CompanyName.Notebook.NoteTaking.Infrastructure.Server;
     using NSubstitute;
     using NUnit.Framework;
@@ -12,6 +13,7 @@ namespace Test.Unit.Infrastructure.Server
     {
         private INoteFactory _noteFactory;
         private ISubscriberFactory _subscriberFactory;
+        private ICategoryRepository _categoryRepository;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
@@ -19,6 +21,7 @@ namespace Test.Unit.Infrastructure.Server
             // ARRANGE
             _noteFactory = Substitute.For<INoteFactory>();
             _subscriberFactory = Substitute.For<ISubscriberFactory>();
+            _categoryRepository = Substitute.For<ICategoryRepository>();
         }
 
         [OneTimeTearDown]
@@ -42,7 +45,11 @@ namespace Test.Unit.Infrastructure.Server
             // ARRANGE
 
             // ACT
-            var subjectUnderTest = new NoteTaker();
+            var subjectUnderTest = new NoteTaker(
+                _noteFactory,
+                _subscriberFactory,
+                _categoryRepository
+            );
 
             // ASSERT
             Assert.That(subjectUnderTest, Is.Not.Null);
