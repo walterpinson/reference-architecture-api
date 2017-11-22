7f3076bad4abf42b9d5b05f0f32bbc5175ff23d1
Walter Pinson <walter@walterpinson.com>
2017-11-21 22:39:35 -0500

    Take first pass at a Note test harness.

---
+++ test/Test.Unit.Core/Domain/Models/NoteTester.cs
@@ -0,0 +1,40 @@
+namespace Test.Unit.Core.Domain.Models
+{
+    using System;
+    using CompanyName.Notebook.NoteTaking.Core.Domain.Models;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NoteTester
+    {
+        [Test]
+        public void CanCreateNote()
+        {
+            // ARRANGE
+            var expectedText = "Don't to forget to pick up bread from the grocery store.";
+
+            // ACT
+            var subjectUnderTest = new Note(expectedText);
+
+            // ASSERT
+            Assert.That(subjectUnderTest, Is.TypeOf(typeof(Note)));
+            Assert.That(subjectUnderTest.Text, Is.EqualTo(expectedText));
+            Assert.That(subjectUnderTest.Id, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void DoesThrowArgumentExceptionDefaultConstructor()
+        {
+            // ARRANGE
+            var expectedExceptionMessage = "New Note must have text.\nParameter name: text";
+            // ACT
+            // ASSERT
+            var ex = Assert.Throws<ArgumentException>(
+                () => new Note(null)
+            );
+            Assert.That(ex.Message, Is.EqualTo(expectedExceptionMessage));
+        }
+
+    }
+}
