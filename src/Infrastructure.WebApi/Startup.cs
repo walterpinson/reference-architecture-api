4f7a793188e78a917b9ab98907f6345d4dd26014
Walter Pinson <walter@walterpinson.com>
2017-11-23 06:16:25 -0500

    Implement a global exception handler - last line of defense.

--- src/Infrastructure.WebApi/Startup.cs
+++ src/Infrastructure.WebApi/Startup.cs
@@ -3,19 +3,27 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net;
     using System.Threading.Tasks;
+    using CompanyName.Notebook.NoteTaking.Infrastructure.WebApi.Exceptions;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
+    using Newtonsoft.Json;
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILogger<NoteBookExceptionFilter> _logger;
+
+        public Startup(IConfiguration configuration, ILogger<NoteBookExceptionFilter> logger)
         {
-            Configuration = configuration;
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public IConfiguration Configuration { get; }
@@ -32,6 +40,22 @@
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(options => {
+                    options.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        var ex = context.Features.Get<IExceptionHandlerFeature>();
+                        if (ex != null)
+                        {
+                            _logger.LogError(0, ex.Error, "Unhandled exception in Note Taker service.");
+                            var result = JsonConvert.SerializeObject(new { error = ex.Error.Message, innerError = ex.Error.InnerException.Message });
+                            await context.Response.WriteAsync(result).ConfigureAwait(false);
+                        }
+                    });
+                });
             }
 
             app.UseMvc();
