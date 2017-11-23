6634fda40b15692f0b628aeb3de24915e44872d2
Walter Pinson <walter@walterpinson.com>
2017-11-22 19:39:49 -0500

    Add a category factory.  Include methods for Create and Build.

---
+++ src/Core/Domain/Factories/ICategoryFactory.cs
@@ -0,0 +1,16 @@
+namespace CompanyName.Notebook.NoteTaking.Core.Domain.Factories
+{
+    using System;
+    using CompanyName.Notebook.NoteTaking.Core.Domain.Models;
+
+    public interface ICategoryFactory
+    {
+        ICategory Create(
+            string name,
+            INoteFactory noteFactory,
+            ISubscriberFactory subscriberFactory
+        );
+
+        ICategory Build(ICategory category);
+    }
+}
