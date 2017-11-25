995f801e167e4fa5d1464371ce51b0e05356d5dd
Walter Pinson <walter@walterpinson.com>
2017-11-24 21:46:51 -0500

    Rename ISubscriber service and implementation to IRegistrar. Registar is a better metaphor for a construct responsible for enrollment or subscribing.  And since there is already a Subscriber model, this elminates the conflict.

--- src/Core/Application/Services/IRegistrar.cs
+++ src/Core/Application/Services/IRegistrar.cs
@@ -3,9 +3,11 @@ namespace CompanyName.Notebook.NoteTaking.Core.Application.Services
     using System;
     using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
 
-    public interface ISubscriber
+    public interface IRegistrar
     {
         SubscriberDto Subscribe(string emailAddress);
+        SubscriberDto Subscribe(Guid categoryId, string emailAddress);
         void Unsubscribe(Guid subscriberId);
+        void Unsubscribe(Guid categoryId, Guid subscriberId);
     }
 }
--- src/Infrastructure.Server/Registrar.cs
+++ src/Infrastructure.Server/Registrar.cs
@@ -4,14 +4,24 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.Server
     using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
     using CompanyName.Notebook.NoteTaking.Core.Application.Services;
 
-    public class Subscriber : ISubscriber
+    public class Registrar : IRegistrar
     {
         public SubscriberDto Subscribe(string emailAddress)
         {
             throw new NotImplementedException();
         }
 
+        public SubscriberDto Subscribe(Guid categoryId, string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Unsubscribe(Guid subscriberId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(Guid categoryId, Guid subscriberId)
         {
             throw new NotImplementedException();
         }
