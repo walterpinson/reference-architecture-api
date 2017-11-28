c927f364048afc24197fe932ea5847f9b721aea0
Walter Pinson <walter@walterpinson.com>
2017-11-28 01:32:55 -0500

    Add a couple of constructors to support different usge models.

--- src/Infrastructure.Data.MongoDb/CategoryRepository.cs
+++ src/Infrastructure.Data.MongoDb/CategoryRepository.cs
@@ -2,13 +2,23 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.Data.MongoDb
 {
     using System;
     using System.Collections.Generic;
+    using AutoMapper;
     using CompanyName.Notebook.NoteTaking.Core.Domain.Models;
     using CompanyName.Notebook.NoteTaking.Core.Domain.Services;
     using CompanyName.Notebook.NoteTaking.Infrastructure.Data.MongoDb.Models;
+    using MongoDB.Driver;
     using MongoRepository.NetCore;
 
     public class CategoryRepository : MongoRepository<MongoCategory, Guid>, ICategoryRepository
     {
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(MongoUrl mongoUrl, IMapper mapper)
+            : base(mongoUrl) => _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+        public CategoryRepository(string connectionString, IMapper mapper)
+            : base(connectionString) => _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
         public ICategory Add(ICategory category)
         {
             throw new NotImplementedException();
