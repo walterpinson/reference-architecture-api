91692e099f65d565b44ed447f7d631ac44dc3c74
Walter Pinson <walter@walterpinson.com>
2017-11-30 04:17:29 -0500

    Add template script for setting local environment variable configuration overrides.

---
+++ sh/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ASPNETCORE_ENVIRONMENT=NONSENSE_VALUE
+export NoteTaker__NotificationService__SendGrid__ApiKey=NONSENSE_VALUE
+export ConnectionStrings__NoteTakerService=NONSENSE_VALUE
+export ConnectionStrings__NoteTakerTest=NONSENSE_VALUE
