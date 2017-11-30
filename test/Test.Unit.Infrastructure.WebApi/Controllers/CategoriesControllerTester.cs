080c03c3d974c52c5d23eaa9617d73512fb5c4d6
Walter Pinson <walter@walterpinson.com>
2017-11-29 19:13:24 -0500

    Cover most of the categories controller with unit tests.

--- test/Test.Unit.Infrastructure.WebApi/Controllers/CategoriesControllerTester.cs
+++ test/Test.Unit.Infrastructure.WebApi/Controllers/CategoriesControllerTester.cs
@@ -1,5 +1,8 @@
 namespace Test.Unit.Infrastructure.WebApi.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
     using CompanyName.Notebook.NoteTaking.Core.Application.Services;
     using CompanyName.Notebook.NoteTaking.Infrastructure.WebApi.Controllers.v1;
     using Microsoft.Extensions.Logging;
@@ -22,6 +25,65 @@ namespace Test.Unit.Infrastructure.WebApi.Controllers
             // ASSERT
             Assert.That(subjectUnderTest, Is.Not.Null);
             Assert.That(subjectUnderTest, Is.TypeOf(typeof(CategoriesController)));
+        }
+
+        [Test]
+        public void CanGetCategories()
+        {
+            // ARRANGE
+            var noteTaker = Substitute.For<INoteTaker>();
+            var logger = Substitute.For<ILogger<CategoriesController>>();
+            var subjectUnderTest = new CategoriesController(noteTaker, logger);
+
+            var expectedCategoryDtos = Substitute.For<IList<CategoryDto>>();
+
+            noteTaker.ListCategories().Returns(expectedCategoryDtos);
+
+            // ACT
+            var result = subjectUnderTest.Get();
+
+            // ASSERT
+            Assert.That(result, Is.Not.Null);
+            noteTaker.Received(1).ListCategories();
+        }
+
+        [Test]
+        public void CanCreateCategory()
+        {
+            // ARRANGE
+            var noteTaker = Substitute.For<INoteTaker>();
+            var logger = Substitute.For<ILogger<CategoriesController>>();
+            var subjectUnderTest = new CategoriesController(noteTaker, logger);
+
+            var newCategoryMessage = Substitute.For<NewCategoryMessage>();
+            var expectedCategoryDto = Substitute.For<CategoryDto>();
+
+            noteTaker.CreateNewCategory(newCategoryMessage).Returns(expectedCategoryDto);
+
+            // ACT
+            var result = subjectUnderTest.Post(newCategoryMessage);
+
+            // ASSERT
+            Assert.That(result, Is.Not.Null);
+            noteTaker.Received(1).CreateNewCategory(newCategoryMessage);
+        }
+
+        [Test]
+        public void CanDeleteCategory()
+        {
+            // ARRANGE
+            var noteTaker = Substitute.For<INoteTaker>();
+            var logger = Substitute.For<ILogger<CategoriesController>>();
+            var subjectUnderTest = new CategoriesController(noteTaker, logger);
+
+            var expectedId = Guid.NewGuid();
+
+            // ACT
+            var result = subjectUnderTest.Delete(expectedId);
+
+            // ASSERT
+            Assert.That(result, Is.Not.Null);
+            noteTaker.Received(1).RemoveCategory(expectedId);
         }
     }
 }
