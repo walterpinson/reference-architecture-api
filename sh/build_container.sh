d1768c28cad2ced7ac8690456baa5c586c3c3d71
Walter Pinson <walter@walterpinson.com>
2017-11-30 04:05:48 -0500

    Create a script that will build (and run) the docker container.

---
+++ sh/build_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Kill and remove the docker container
+docker rm -f refarch_api
+
+# Run the docker image with all of the necessary overrides
+docker run  -itd -p 5001:80 \
+-e ASPNETCORE_ENVIRONMENT \
+-e NoteTaker__NotificationService__SendGrid__ApiKey \
+-e ConnectionStrings__NoteTakerService \
+--name refarch_api walterpinson/reference-architecture-api
