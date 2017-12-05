b0bd22d43863d53b82872815f719bdf010586bad
Walter Pinson <walter@walterpinson.com>
2017-12-05 12:41:22 -0500

    Add Bson classmap method to the existing MongoMappingProfile. This ensures that the Bson mapping will occur exactly once per application instance.

--- src/Infrastructure.Data.MongoDb/MongoMappingProfile.cs
+++ src/Infrastructure.Data.MongoDb/MongoMappingProfile.cs
@@ -4,12 +4,15 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.Data.MongoDb
     using AutoMapper;
     using CompanyName.Notebook.NoteTaking.Core.Domain.Models;
     using CompanyName.Notebook.NoteTaking.Infrastructure.Data.MongoDb.Models;
+    using MongoDB.Bson.Serialization;
 
     public class MongoMappingProfile : Profile
     {
         public MongoMappingProfile()
         {
             CreateMap<Category, MongoCategory>().ReverseMap();
+            
+            BsonClassMap.RegisterClassMap<Note>();
         }
     }
 }
