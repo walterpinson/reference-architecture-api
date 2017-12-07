c19b25aaceec23ed549d5306560a912e1923d811
Walter Pinson <walter@walterpinson.com>
2017-12-07 02:32:03 -0500

    Resolve stale image issue. Image was not getting rebuilt with the latest code durng the builds.  So now we are manually removing the image in order to force a rebuild.

--- sh/build_stack.sh
+++ sh/build_stack.sh
@@ -4,5 +4,8 @@
 docker rm -f stack_refarch_api
 docker rm -f stack_refarch_ods
 
+# Remove the docker images
+docker rmi walterpinson/reference-architecture-api:stackhouse --force
+
 # Run the Stack
 docker-compose up --remove-orphans
