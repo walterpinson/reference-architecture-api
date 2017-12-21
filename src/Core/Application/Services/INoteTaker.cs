431b5cd2e9e44a66e2f3e57cf2898c8c883311f6
Walter Pinson <walter@walterpinson.com>
2017-12-21 12:41:38 -0500

    Make all application service calls require that a security context object be passed in.

--- src/Core/Application/Services/INoteTaker.cs
+++ src/Core/Application/Services/INoteTaker.cs
@@ -8,22 +8,22 @@ namespace CompanyName.Notebook.NoteTaking.Core.Application.Services
     {
         // Work with notes with regard for categories.
         // In esseence these notes will be stored in the context of default category.
-        NoteDto TakeNote(NewNoteMessage newNoteMessage);
-        NoteDto ReadNote(Guid noteId);
-        void RemoveNote(Guid noteId);
-        IList<NoteDto> ListNotes();
+        NoteDto TakeNote(SecurityContext securityContext, NewNoteMessage newNoteMessage);
+        NoteDto ReadNote(SecurityContext securityContext, Guid noteId);
+        void RemoveNote(SecurityContext securityContext, Guid noteId);
+        IList<NoteDto> ListNotes(SecurityContext securityContext, );
 
         // Work with categories
-        IList<CategoryDto> ListCategories();
-        CategoryDto CreateNewCategory(NewCategoryMessage newCategoryMessage);
-        CategoryDto RenameCategory(Guid categoryId, string newCategoryName);
-        CategoryDto GetCategoryDetail(Guid categoryId);
-        void RemoveCategory(Guid categoryId);
+        IList<CategoryDto> ListCategories(SecurityContext securityContext, );
+        CategoryDto CreateNewCategory(SecurityContext securityContext, NewCategoryMessage newCategoryMessage);
+        CategoryDto RenameCategory(SecurityContext securityContext, Guid categoryId, string newCategoryName);
+        CategoryDto GetCategoryDetail(SecurityContext securityContext, Guid categoryId);
+        void RemoveCategory(SecurityContext securityContext, Guid categoryId);
 
         // Work with categorized notes
-        NoteDto TakeCategorizedNote(Guid categoryId, NewNoteMessage newNoteMessage);
-        CategoryDto RemoveCategorizedNote(Guid categoryId, Guid noteId);
-        IList<NoteDto> ListCategorizedNotes(Guid categoryId);
-        NoteDto ReadCategorizedNote(Guid categoryId, Guid noteId);
+        NoteDto TakeCategorizedNote(SecurityContext securityContext, Guid categoryId, NewNoteMessage newNoteMessage);
+        CategoryDto RemoveCategorizedNote(SecurityContext securityContext, Guid categoryId, Guid noteId);
+        IList<NoteDto> ListCategorizedNotes(SecurityContext securityContext, Guid categoryId);
+        NoteDto ReadCategorizedNote(SecurityContext securityContext, Guid categoryId, Guid noteId);
     }
 }
