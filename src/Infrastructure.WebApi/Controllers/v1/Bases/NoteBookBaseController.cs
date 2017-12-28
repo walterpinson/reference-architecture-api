88d497839a2aeb57348680483fbb30c1ef8a4a2d
Walter Pinson <walter@walterpinson.com>
2017-12-27 20:15:17 -0500

    Resolve error in base controller.

--- src/Infrastructure.WebApi/Controllers/v1/Bases/NoteBookBaseController.cs
+++ src/Infrastructure.WebApi/Controllers/v1/Bases/NoteBookBaseController.cs
@@ -3,16 +3,21 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.WebApi.Controllers.v1.B
     using System;
     using CompanyName.Notebook.NoteTaking.Core.Application.Messages;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Primitives;
 
     public abstract class NoteBookBaseController : Controller
     {
         protected SecurityContext SecurityContext
         {
             get {
+                StringValues authHeader;
+                var authHeaderExists = HttpContext.Request.Headers.TryGetValue("Authorization", out authHeader);
+                var token = (authHeaderExists) ? authHeader.ToString().Split(' ')[1] : string.Empty;
+
                 var securityContext = new SecurityContext
                 {
                     UserId = User.Identity.Name,
-                    Token = HttpContext.Request.Headers["Authorization"].ToString().Split(' ')[1],
+                    Token = token,
                     ApiRoute = HttpContext.Request.Path + HttpContext.Request.QueryString,
                     HttpAction = HttpContext.Request.Method
                 };
