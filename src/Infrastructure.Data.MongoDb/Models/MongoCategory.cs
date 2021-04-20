c7699fce9880b4b5b5b4d2325350ac75e8a6bc91
Walter Pinson <walter@walterpinson.com>
2021-04-19 21:34:04 -0400

    #10. Update MongoRepository namespace reference.

--- src/Infrastructure.Data.MongoDb/Models/MongoCategory.cs
+++ src/Infrastructure.Data.MongoDb/Models/MongoCategory.cs
@@ -2,7 +2,7 @@ namespace CompanyName.Notebook.NoteTaking.Infrastructure.Data.MongoDb.Models
 {
     using System;
     using CompanyName.Notebook.NoteTaking.Core.Domain.Models;
-    using MongoRepository.NetCore;
+    using MongoRepository.Net5;
 
     [CollectionName("Categories")]
     public class MongoCategory : Category, IEntity<Guid>
